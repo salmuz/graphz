a4d569166d8efbd931bcc0f9fc0a37b329894f1d
salmuz <salmuz@gmail.com>
2013-01-16 16:56:38 +0100

    Projet des  Graphes avec composant

---
+++ sources/project-m1info-vue-graphe/m1info-graphe-mvp/src/test/java/fr/edu/bp/m1info/App.java
@@ -0,0 +1,13 @@
+package pe.org.salmuz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ sources/project-m1info-vue-graphe/m1info-graphe-structure/src/main/java/fr/edu/bp/m1info/structure/Point.java
@@ -0,0 +1,32 @@
+package fr.edu.bp.m1info.structure;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: salmuz
+ * Date: 26/12/12
+ * Time: 19:47
+ * To change this template use File | Settings | File Templates.
+ */
+public abstract class Point {
+    private double x;
+    private double y;
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+}
+
+
+
